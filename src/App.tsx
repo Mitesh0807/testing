2495796b00aa37ca557626481bce30d6ed8cd710
Mitesh savaliya <msavaliya@plusonex.com>
2024-03-15 18:10:48 +0530

    yt video show

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import YouTubePlayer from './Youtubeplayer'
+function App() {
+  return (
+    <>
+      <YouTubePlayer videoUrl="https://youtu.be/8Iic7y3y0FE?si=tzG1F32GgnWElI9e" />
+    </>
+  )
+}
+
+export default App
